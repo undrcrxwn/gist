3ea84637c3a6b349aa2b78953b9941776b027cbd
gгёzы <69521267+undrcrxwn@users.noreply.github.com>
2022-10-30 20:49:56 +0300

    Update PersistentAsync.cs

--- PersistentAsync.cs
+++ PersistentAsync.cs
@@ -1,111 +1,71 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-namespace Playground;
-
 public static class Program
 {
     public static async Task Main()
     {
         var handler = new Handler();
-        await handler.ExecuteAsync(42);
+
+        var context = new SynchronizationContextWrapper();
+        SynchronizationContext.SetSynchronizationContext(context);
+        await handler.ExecuteAsync(95274356, 3452345, 42);
+    }
+}
+
+public class SynchronizationContextWrapper : SynchronizationContext
+{
+    private const string StateMachinePropertyName = "StateMachine";
+    private bool _isStateRetrieved;
+    
+    public string? StateKey;
+
+    public override void Post(SendOrPostCallback callback, object? state)
+    {
+        if (!_isStateRetrieved)
+        {
+            var machine = state!.GetType()
+                .GetField(StateMachinePropertyName)!
+                .GetValue(state)!;
+
+            var field = machine.GetType()
+                .GetField("<>1__state", BindingFlags.Instance | BindingFlags.Public)!;
+            field.SetValue(machine, 2);
+
+            Console.WriteLine($"Retrieving state by key '{StateKey}'");
+
+            _isStateRetrieved = true;
+        }
+
+        base.Post(callback, state);
     }
 }
 
 public class Handler
 {
-    public async Task ExecuteAsync(int x)
+    public async Task ExecuteAsync(long chatId, long userId, int x)
     {
-        Console.WriteLine(x);
-        
-        await IncrementX();
-        Console.WriteLine(x);
-        
-        await IncrementX();
-        Console.WriteLine(x);
-        
-        await IncrementX();
-        Console.WriteLine(x);
-        
-        await IncrementX();
-        Console.WriteLine(x);
+        await Async.RetrieveState($"{chatId}-{userId}");
+
+        Console.WriteLine($"1: {x}");
+        await Task.Yield();
+        Console.WriteLine($"2: {x}");
+        await Task.Yield();
+        Console.WriteLine($"3: {x}");
+        await Task.Yield();
+        Console.WriteLine($"4: {x}");
+        await Task.Yield();
+        Console.WriteLine($"5: {x}");
     }
-
-    private static IAwaitable IncrementState() => Async.CreateAsyncStateMachineHandlingAwaitable(machine =>
-    {
-        var field = machine.GetType().GetField("<>1__state", BindingFlags.Instance | BindingFlags.Public)!;
-        var value = (int)field.GetValue(machine)!;
-        field.SetValue(machine, value + 3);
-    });
-
-    private static IAwaitable IncrementX() => Async.CreateAsyncStateMachineHandlingAwaitable(machine =>
-    {
-        var field = machine.GetType().GetField("x", BindingFlags.Instance | BindingFlags.Public)!;
-        var value = (int)field.GetValue(machine)!;
-        field.SetValue(machine, value + 1);
-    });
-
-    private static IAwaitable PrintMachineType() => Async.CreateAsyncStateMachineHandlingAwaitable(machine =>
-        Console.WriteLine(machine.GetType().Name));
 }
 
 public class Async
 {
-    public static IAwaitable CreateAsyncStateMachineHandlingAwaitable(Action<IAsyncStateMachine> stateMachineHandler)
+    public static YieldAwaitable RetrieveState(string stateKey)
     {
-        var awaiter = new YieldingAwaiter(continuation =>
-        {
-            var machine = GetStateMachine(continuation);
-            stateMachineHandler(machine);
-        });
-
-        return new Awaitable(awaiter);
+        var context = (SynchronizationContextWrapper)SynchronizationContext.Current!;
+        context.StateKey = stateKey;
+        
+        return Task.Yield();
     }
-
-    private static IAsyncStateMachine GetStateMachine(Action continuation)
-    {
-        var target = continuation.Target!;
-        var field = target.GetType().GetField("StateMachine", BindingFlags.Public | BindingFlags.Instance)!;
-        return (IAsyncStateMachine)field.GetValue(target)!;
-    }
-}
-
-public struct YieldingAwaiter : IAwaiter
-{
-    private readonly Action<Action> _continuationHandler;
-    private Action? _continuation;
-
-    public YieldingAwaiter(Action<Action> continuationHandler) =>
-        _continuationHandler = continuationHandler;
-
-    public bool IsCompleted => _continuation is not null;
-
-    public void GetResult() => _continuationHandler(_continuation!);
-
-    public void OnCompleted(Action continuation)
-    {
-        _continuation = continuation;
-        Task.Run(continuation);
-    }
-}
-
-public interface IAwaitable
-{
-    IAwaiter GetAwaiter();
-}
-
-public interface IAwaiter : INotifyCompletion
-{
-    bool IsCompleted { get; }
-    void GetResult();
-}
-
-public class Awaitable : IAwaitable
-{
-    private readonly IAwaiter _awaiter;
-
-    internal Awaitable(IAwaiter awaiter) =>
-        _awaiter = awaiter;
-
-    public IAwaiter GetAwaiter() => _awaiter;
 }
